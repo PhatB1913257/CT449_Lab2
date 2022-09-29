f3fe3f22ee3ceea54e5801c97df7cd62956e48bf
phatmorder <thanhphatoffical@gmail.com>
2022-09-29 15:21:54 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
     port: process.env.PORT || 3000,
+    },
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb+srv://phatto:Phat010501@cluster0.hdjxxpq.mongodb.net/ct449_b1913257?retryWrites=true&w=majority"
     }
 };
     module.exports = config;
